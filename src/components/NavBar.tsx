7f500e0ce98906c37c3f105eeb6bfc52c9f28af8
rbc33 <ricardobenthem@gmail.com>
2025-11-22 13:35:22 +0100

    useTheme, and toggleTheme

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,11 @@
 import { Link, NavLink } from 'react-router-dom'
 import logo from '../assets/logo.png'
+import ThemeToggle from './ThemeToggle'
 
 const NavBar = () => {
+  const isActiveStyle = ({ isActive }:{isActive: boolean}) =>
+					isActive ? ' text-slate-500 dark:text-slate-400' : 'hover:text-slate-500 dark:hover:text-slate-400!'
+				
   return (
     <div className='flex justify-between items-center border-b-2 border-slate-600'>
         <Link to="/" >
@@ -11,23 +15,18 @@ const NavBar = () => {
         <h1 className='text-center text-3xl font-bold'>Iron Apts</h1>
         </div>
         </Link>
-        <div className='grow'></div>
+        <div className='grow flex items-center'></div>
         <NavLink
-				className={({ isActive }) =>
-					isActive ? 'text-slate-400' : 'hover:text-slate-400!'
-				}
+				className={isActiveStyle}
 				to="/"
 			><button className='text-2xl  mt-5 mr-5'>Home</button></NavLink>
         <NavLink
-				className={({ isActive }) =>
-					isActive ? 'text-slate-400' : 'hover:text-slate-400!'
-				}
+				className={isActiveStyle}
 			 to="/newbooking"><button className='text-2xl mt-5 mr-5'>New Booking</button></NavLink>
         <NavLink
-				className={({ isActive }) =>
-					isActive ? 'text-slate-400' : 'hover:text-slate-400!'
-				}
+				className={isActiveStyle}
 			 to="/about"><button className='text-2xl  mt-5 mr-5'>About</button></NavLink>
+       <ThemeToggle />
         </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from './context/theme.context.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <ThemeProvider>
     <App />
+    </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
