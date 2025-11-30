4a96f0c822db78cc05df61d1283f662944417cbb
rbc33 <ricardobenthem@gmail.com>
2025-11-30 21:37:36 +0100

    fix Booking card date

--- src/components/BookingCard.tsx
+++ src/components/BookingCard.tsx
@@ -1,21 +1,34 @@
-import type { Booking } from '../types/types'
+import type { Booking } from "../types/types";
 
 interface BookingCardProps {
-  booking: Booking
-    handleDelete: (bookingId: number) => void
+  booking: Booking;
+  handleDelete: (bookingId: number) => void;
 }
 
-const BookingCard = ({booking, handleDelete}:BookingCardProps) => {
+const BookingCard = ({ booking, handleDelete }: BookingCardProps) => {
   return (
-    <div key={booking.id} className='flex flex-col items-start justify-self-start'>
-                                    <div className=" card border-2 border-slate-600 p-3 mt-2  w-[400px]">
-                                        <p className="text-xl">Guest Name: {booking.guestName}</p>
-                                        <p className="text-xl">Guests: {booking.guests}</p>
-                                        <p className="text-xl">From: {booking.in} </p><p className="text-xl">To: {booking.out}</p>
-                                    <button className='btn btn-secondary mt-2 justify-self-start' onClick={() => handleDelete(booking.id)}>Delete</button>
-                                    </div>
-                                    </div>
-  )
-}
+    <div
+      key={booking.id}
+      className="flex flex-col items-start justify-self-start"
+    >
+      <div className=" card border-2 border-slate-600 p-3 mt-2  w-[400px]">
+        <p className="text-xl">Guest Name: {booking.guestName}</p>
+        <p className="text-xl">Guests: {booking.guests}</p>
+        <p className="text-xl">
+          From: {new Date(booking.in).toLocaleDateString("en-GB")}
+        </p>
+        <p className="text-xl">
+          To: {new Date(booking.out).toLocaleDateString("en-GB")}
+        </p>
+        <button
+          className="btn btn-secondary mt-2 justify-self-start"
+          onClick={() => handleDelete(booking.id)}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default BookingCard
+export default BookingCard;
