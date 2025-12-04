2ac1cabf40b6587c1ff971f04d82e33f18a81e5a
rbc33 <ricardobenthem@gmail.com>
2025-12-04 17:45:24 +0100

    Burguer menu to the right side

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,28 +12,10 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-100 shadow-md mb-5 rounded-box">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <GiHamburgerMenu className="text-xl" />
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-1 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <NavLink to="/"><p className="text-xl">Home</p></NavLink>
-            </li>
-            <li>
-              <NavLink to="/newbooking"><p className="text-xl">New Booking</p></NavLink>
-            </li>
-            <li>
-              <NavLink to="/about"><p className="text-xl">About</p></NavLink>
-            </li>
-          </ul>
-        </div>
-        <Link to="/" className="btn btn-ghost text-xl gap-2">
+        
+        <Link to="/" className="btn btn-ghost text-2xl gap-2">
           <img src={logo} alt="Logo" className="w-8 h-8 rounded-lg" />
-          Iron Apts
+          IronApts
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
@@ -58,6 +40,25 @@ const NavBar = () => {
       <div className="navbar-end">
         <ThemeToggle />
       </div>
+      <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <GiHamburgerMenu className="text-xl" />
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-1 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <NavLink to="/"><p className="text-xl">Home</p></NavLink>
+            </li>
+            <li>
+              <NavLink to="/newbooking"><p className="text-xl">New Booking</p></NavLink>
+            </li>
+            <li>
+              <NavLink to="/about"><p className="text-xl">About</p></NavLink>
+            </li>
+          </ul>
+        </div>
     </div>
   );
 };
